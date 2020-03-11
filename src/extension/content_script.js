e9140b67bf7e930d87a1bfd0c90097f9c0a26b0d
Oscar Mattsson <oscar_mattsson@live.se>
2020-03-11 12:05:48 +0100

    Added skeleton for content script identifying input targets

--- src/extension/content_script.js
+++ src/extension/content_script.js
@@ -1,1 +1,13 @@
-console.log("Hello content");
+let types = /text|url|password|email/i;
+let ids = /user|name|pass|mail|url|server|site/i;
+
+let forms = document.forms;
+
+for(let i = 0; i < forms.length; i++) {
+  for(let j = 0; j < forms[i].length; j++) {
+    let element = forms[i][j];
+    if (types.test(element.type) && (ids.test(element.id) || ids.test(element.name))) {
+      console.log(element);
+    }
+  }
+}
