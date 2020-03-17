d3e5a18feb485c6ec18b7a2bb2796ab8019d432f
Oscar Mattsson <oscar_mattsson@live.se>
2020-03-17 16:49:04 +0100

    Created template for user interface.

--- src/extension/pages/Popup.js
+++ src/extension/pages/Popup.js
@@ -1,55 +1,91 @@
 import React, { useState, useEffect } from 'react';
+import Auth from '../containers/Auth';
+import logo from '../../assets/logo.png';
+import './Popup.scss';
 
 export default function Popup(props) {
-  const [token, setToken] = useState(null);
-  const [fields, setFields] = useState(null);
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setToken(event.target.token.value);
-  };
-
-  const handleMessage = (request, sender) => {
-    setFields(Object.keys(request.fields).map(key => {
-      if (/pass/.test(key)) {
-        return <li key={key}>{request.fields[key].replace(/./g,'*')}</li>;
-      }
-      return <li key={key}>{request.fields[key]}</li>;
-    }));
-  };
-
-  useEffect(() => {
-    browser.runtime.onMessage.addListener(handleMessage);
-  }, []);
-
-  useEffect(() => {
-    browser.storage.local.get('token')
-      .then(values => {
-        if(values.token !== undefined) {
-          setToken(values.token.value);
-        }
-      });
-  }, []);
-
-  useEffect(() => {
-    browser.storage.local.set({
-      token: {
-        value: token,
-        lastRefresh: Date.now(),
-      },
-    });
-  }, [token]);
-
   return (
-    <section>
-      <p>Token: {token}</p>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="token" id="token" />
-        <input type="submit" />
-      </form>
-      <ul>
-        {fields}
-      </ul>
+    <section className="popup">
+      <header><img src={logo} alt="StoredSafe" /></header>
+      <section className="login">
+        <select>
+          <option>YubiKey</option>
+          <option>TOTP</option>
+        </select>
+        <form>
+          <label for="username">Username or e-mail</label>
+          <input
+            type="text"
+            name="username"
+            id="username" />
+          <ul className="login-type">
+            <li key="yubikey">
+              <label for="passphrase">Passphrase + YubiKey</label>
+              <input
+                type="password"
+                name="passphrase"
+                id="passphrase" />
+            </li>
+            <li key="totp">
+              <label for="passphrase">Passphrase</label>
+              <input
+                type="password"
+                name="passphrase"
+                id="passphrase" />
+              <label for="otp">OTP</label>
+              <input
+                type="text"
+                name="OTP"
+                id="OTP" />
+            </li>
+          </ul>
+          <label for="rememberUsername">
+            <input
+              type="checkbox"
+              name="rememberUsername"
+              id="rememberUsername" />
+            Remember Username
+          </label>
+          <input type="submit" value="Login to StoredSafe" />
+        </form>
+      </section>
+      <section className="list">
+        <input type="text" placeholder="Search" />
+        <ul>
+          <li className="vault">
+            My Vault
+            <ul>
+              <li className="object">
+                safe.storedsafe.com
+                <ul>
+                  <li className="field">
+                    <span className="key">Username: </span>
+                    <span className="value">john.doe@example.com</span>
+                  </li>
+                  <li className="field crypted">
+                    <span className="key">Passphase: </span>
+                    <span className="value">****************</span>
+                    <div className="button">Copy</div><div className="button">Reveal</div>
+                  </li>
+                </ul>
+                <li className="object">Safe PIN</li>
+                <ul>
+                  <li className="field crypted">
+                    <span className="key">Pin: </span>
+                    <span className="value">******</span>
+                    <div className="button">Copy</div><div className="button">Reveal</div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+      <section className="save">
+      </section>
+      <section className="logout">
+        <button>Logout from StoredSafe</button>
+      </section>
     </section>
   );
 }
