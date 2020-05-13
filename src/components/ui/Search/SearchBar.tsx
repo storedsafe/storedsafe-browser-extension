d8a100adfcd72119728af77563d821d2b4d3e572
Oscar Mattsson <oscar_mattsson@live.se>
2020-05-13 10:53:18 +0200

    Extracted svg elements into module

--- src/components/ui/Search/SearchBar.tsx
+++ src/components/ui/Search/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import svg from '../../../ico/svg';
 import './SearchBar.scss';
 
 export interface SearchBarProps {
@@ -31,7 +32,7 @@ export const SearchBar: React.FunctionComponent<SearchBarProps> = ({
             }}
           />
           <button className="search-bar-button" type="submit">
-            <svg className="search-bar-icon" width="40" height="40" version="1.1" viewBox="0 0 10.583 10.583" xmlns="http://www.w3.org/2000/svg"><path transform="scale(.26458)" d="m27.486 0a12.5 12.5 0 00-12.486 12.5 12.5 12.5 0 002.3145 7.2441 12.5 12.5 0 000 .001953l-17.314 17.312 2.9414 2.9414 17.312-17.312a12.5 12.5 0 007.2461 2.3125 12.5 12.5 0 0012.5-12.5 12.5 12.5 0 00-12.5-12.5 12.5 12.5 0 00-.013672 0zm-.11914 3.6641a8.8361 8.8361 0 01.13281 0 8.8361 8.8361 0 018.8359 8.8359 8.8361 8.8361 0 01-8.8359 8.8359 8.8361 8.8361 0 01-8.8359-8.8359 8.8361 8.8361 0 018.7031-8.8359z"/></svg>
+            {svg.search}
           </button>
         </div>
       </label>
