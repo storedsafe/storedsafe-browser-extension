fa936dbd2c96730338204e75149bb0c6eabf47d4
Oscar Mattsson <oscar_mattsson@live.se>
2020-08-07 10:43:03 +0200

    Added username and hovertext to search results (#8)

--- src/components/Search/layout/SearchTitle.tsx
+++ src/components/Search/layout/SearchTitle.tsx
@@ -10,14 +10,19 @@ interface SearchTitleProps {
 export const SearchTitle: React.FunctionComponent<SearchTitleProps> = ({
   host,
   result
-}: SearchTitleProps) => (
-  <article
-    style={{ backgroundImage: `url('${icons[result.icon]}')` }}
-    className='search-title'
-  >
-    <div className='search-title-text'>
-      <p className='search-title-name'>{result.name}</p>
-      {host !== undefined && <p className='search-title-host'>{host}</p>}
-    </div>
-  </article>
-)
+}: SearchTitleProps) => {
+  const usernameField = result.fields.find(({ name }) => name === 'username')
+  return (
+    <article
+      style={{ backgroundImage: `url('${icons[result.icon]}')` }}
+      className='search-title'
+      title={result.type}
+    >
+      <div className='search-title-text'>
+        <p className='search-title-name'>{result.name}</p>
+        {usernameField && usernameField.value !== result.name && <p className='search-title-username'>{usernameField.value}</p>}
+        {host !== undefined && <p className='search-title-host'>{host}</p>}
+      </div>
+    </article>
+  )
+}
