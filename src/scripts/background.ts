518eb8c6bbe73618541907e426a4f9fbaa098ddb
Oscar Mattsson <oscar_mattsson@live.se>
2020-08-28 14:23:08 +0200

    Cleaned up obsolete code

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -189,101 +189,10 @@ type MessageHandler<T> = (
 const messageHandlers: {
   tabSearch: MessageHandler<void>
   copyToClipboard: MessageHandler<string>
-  submit: MessageHandler<Record<string, string>>
-  toggle: MessageHandler<void>
   [key: string]: MessageHandler<unknown>
 } = {
   tabSearch: async (data, sender) => await tabFind(sender.tab),
   copyToClipboard: async value => await copyToClipboard(value),
-  submit: async (values, sender) => {
-    const { title, url, id } = sender.tab
-
-    // If user is not online, don't offer to save
-    const sessions = await SessionsActions.fetch()
-    if (sessions.size === 0) {
-      console.log('no sessions found, return')
-      return
-    }
-
-    // If site is ignored, don't offer to save
-    const ignore = await IgnoreActions.fetch()
-    if (ignore.includes(url)) return
-
-    // If result with username for site exists in cache, don't offer to save
-    const tabResults = await TabResultsActions.fetch()
-    for (const results of tabResults.get(id).values()) {
-      for (const result of results) {
-        for (const { value } of result.fields) {
-          if (value === undefined) continue
-          if (value.match(urlToNeedle(url)) !== null) {
-            const username = result.fields.find(
-              ({ name }) => name === 'username'
-            ).value
-            if (username === values['username']) {
-              return
-            }
-          }
-        }
-      }
-    }
-
-    // Track current URL so that iframe only appears on the current tab.
-    let currentUrl = url
-    function onTabUpdate (tabId: number, changeInfo: { url: string }) {
-      if (tabId === id && changeInfo.url !== undefined) {
-        currentUrl = changeInfo.url
-      }
-    }
-    browser.tabs.onUpdated.addListener(onTabUpdate)
-
-    let savePort: browser.runtime.Port
-    let injectPort: browser.runtime.Port
-    values = { name: title, url: urlToNeedle(url), ...values }
-    function onConnect (port: browser.runtime.Port) {
-      if (port.sender?.url === browser.runtime.getURL('index.html') + '#save') {
-        if (savePort === undefined) {
-          savePort = port
-          console.log('SEND SAVE FILL')
-          port.postMessage({
-            type: 'save.fill',
-            sender: 'background',
-            data: values
-          })
-        } else if (injectPort) {
-          console.log('SEND CLOSE FILL')
-          injectPort.postMessage({
-            type: 'save.close',
-            sender: 'background'
-          })
-          console.log('disconnect port')
-          port.disconnect()
-          browser.tabs.onUpdated.removeListener(onTabUpdate)
-        }
-      } else if (port.sender?.url === url) {
-        injectPort = port
-        console.log('SEND SAVE OPEN')
-        injectPort.postMessage({
-          type: 'save.open',
-          sender: 'background'
-        })
-      }
-
-      port.onDisconnect.addListener(port => {
-        if (
-          port.sender?.url ===
-          browser.runtime.getURL('index.html') + '#save'
-        ) {
-          savePort = undefined
-        }
-      })
-    }
-    browser.runtime.onConnect.addListener(onConnect)
-  },
-  toggle: async (data, sender) => {
-    browser.tabs.sendMessage(sender.tab.id, {
-      type: 'toggle'
-    })
-  }
 }
 
 /**
