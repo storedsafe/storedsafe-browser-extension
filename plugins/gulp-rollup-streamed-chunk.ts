857d879ab5e4dc2a7405783dcae24ee86243619d
Oscar Mattsson <oscar@storedsafe.com>
2020-10-12 10:39:55 +0200

    Changed to async, improved error handling

--- plugins/gulp-rollup-streamed-chunk.ts
+++ plugins/gulp-rollup-streamed-chunk.ts
@@ -1,10 +1,4 @@
-import {
-  rollup,
-  OutputOptions,
-  RollupOptions,
-  RollupBuild,
-  RollupOutput
-} from 'rollup'
+import { rollup, OutputOptions, RollupOptions } from 'rollup'
 import { Readable } from 'stream'
 const Vinyl = require('vinyl')
 
@@ -13,50 +7,40 @@ export default function (options: RollupOptions | RollupOptions[]) {
     objectMode: true,
     read () {
       const entries = Array.isArray(options) ? options : [options]
-      const bundles: Promise<RollupBuild>[] = []
-      const outputs: Promise<RollupOutput>[] = []
 
-      for (const entry of entries) {
-        // Set up bundles
-        const bundlePromise = rollup(entry)
-        bundles.push(bundlePromise)
-
-        // Generate output
-        const outputPromise = bundlePromise.then(bundle => {
-          return bundle.generate(entry.output as OutputOptions)
-        })
-        outputs.push(outputPromise)
-      }
-
-      // Stream chunks
-      Promise.all(bundles).then(() => {
-        Promise.all(outputs).then(rollupOutputs => {
-          for (const { output } of rollupOutputs) {
-            for (const chunk of output) {
-              if (chunk.type === 'chunk') {
-                const code = new Vinyl({
-                  path: chunk.fileName,
-                  contents: Buffer.from(chunk.code)
-                })
-                this.push(code)
-                if (!!chunk.map) {
-                  const map = code.clone({ contents: false })
-                  map.extname += '.map'
-                  map.contents = Buffer.from(chunk.map.toString())
-                  this.push(map)
-                }
-              } else if (chunk.type === 'asset') {
-                const asset = new Vinyl({
-                  path: chunk.fileName,
-                  contents: Buffer.from(chunk.source)
-                })
-                this.push(asset)
+      const promises = entries.map(async entry => {
+        try {
+          const bundle = await rollup(entry)
+          const { output } = await bundle.generate(
+            entry.output as OutputOptions
+          )
+          for (const chunk of output) {
+            if (chunk.type === 'chunk') {
+              const code = new Vinyl({
+                path: chunk.fileName,
+                contents: Buffer.from(chunk.code)
+              })
+              this.push(code)
+              if (!!chunk.map) {
+                const map = code.clone({ contents: false })
+                map.extname += '.map'
+                map.contents = Buffer.from(chunk.map.toString())
+                this.push(map)
               }
+            } else if (chunk.type === 'asset') {
+              const asset = new Vinyl({
+                path: chunk.fileName,
+                contents: Buffer.from(chunk.source)
+              })
+              this.push(asset)
             }
           }
-          this.push(null)
-        })
+        } catch (error) {
+          this.emit('error', error.message)
+        }
       })
+
+      Promise.all(promises).finally(() => this.push(null))
     }
   })
 }
