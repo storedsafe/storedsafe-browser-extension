1ca9b9e058355c73828b4f8b1fb80f1eac86674e
Oscar Mattsson <oscar@storedsafe.com>
2020-10-21 12:04:13 +0200

    Added logger with log levels

---
+++ src/global/logger.ts
@@ -0,0 +1,69 @@
+export enum LogLevel {
+  NONE = 0,
+  ERROR = 1,
+  WARN = 2,
+  INFO = 3,
+  LOG = 4,
+  DEBUG = 5,
+  ALL = 6
+}
+
+const isProduction = process.env.NODE_ENV !== 'development'
+
+export class Logger {
+  public static level: LogLevel = isProduction ? LogLevel.ERROR : LogLevel.ALL
+  private readonly prefix: string
+
+  public constructor (name: string) {
+    this.group = this.group.bind(this)
+    this.error = this.error.bind(this)
+    this.warn = this.warn.bind(this)
+    this.info = this.info.bind(this)
+    this.log = this.log.bind(this)
+    this.debug = this.debug.bind(this)
+
+    this.prefix = `[${name}]: `
+  }
+
+  public group (name: string, level: LogLevel) {
+    if (Logger.level >= level) {
+      console.group(this.prefix + name)
+    }
+  }
+
+  public groupEnd (level: LogLevel) {
+    if (Logger.level >= level) {
+      console.groupEnd()
+    }
+  }
+
+  public error (msg: any, ...params: any[]) {
+    if (Logger.level >= LogLevel.ERROR) {
+      console.error(this.prefix + msg, ...params)
+    }
+  }
+
+  public warn (msg: any, ...params: any[]) {
+    if (Logger.level >= LogLevel.WARN) {
+      console.warn(this.prefix + msg, ...params)
+    }
+  }
+
+  public info (msg: any, ...params: any[]) {
+    if (Logger.level >= LogLevel.INFO) {
+      console.info(this.prefix + msg, ...params)
+    }
+  }
+
+  public log (msg: any, ...params: any[]) {
+    if (Logger.level >= LogLevel.LOG) {
+      console.log(this.prefix + msg, ...params)
+    }
+  }
+
+  public debug (msg: any, ...params: any[]) {
+    if (Logger.level >= LogLevel.DEBUG) {
+      console.debug(this.prefix + msg, ...params)
+    }
+  }
+}
