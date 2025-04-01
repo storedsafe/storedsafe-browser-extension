6995cee3b52d13a6964d47f7eaa6052747fe5ac0
Oscar Mattsson <oscar@storedsafe.com>
2025-04-01 11:09:39 +0200

    add loading state for checking sessions on startup

--- src/ui/stores/browserstorage/sessionsState.svelte.ts
+++ src/ui/stores/browserstorage/sessionsState.svelte.ts
@@ -2,8 +2,12 @@ import * as auth from "@/global/api/auth";
 import { sessions as sessionsStorage } from "@/global/storage";
 import { loading } from "../loading.svelte";
 import { SvelteMap } from "svelte/reactivity";
+import { Logger } from "@/global/logger";
+
+const logger = new Logger("sessions");
 
 export const SESSIONS_LOADING_ID = "sessions.loading";
+export const SESSIONS_CHECK_LOADING_ID = "sessions.loading";
 export const SESSIONS_LOGIN_LOADING_ID = "sessions.login.totp";
 export const SESSIONS_LOGOUT_LOADING_ID = "sessions.logout";
 export const SESSIONS_CLEAR_LOADING_ID = "sessions.clear";
@@ -15,12 +19,20 @@ class SessionsState {
   constructor() {
     const promise = sessionsStorage.subscribe(this.#update.bind(this));
     loading.add(SESSIONS_LOADING_ID, promise, {
-      onSuccess: (data) => {
-        this.isInitialized = true;
+      onSuccess: async (data) => {
+        logger.debug("Sessions loaded from storage.");
         this.#update(data, this.data);
+        const loadingPromises = [];
         for (const [host, session] of data) {
-          auth.check(host, session.token)
+          // Check that sessions are still alive on startup
+          loadingPromises.push(auth.check(host, session.token));
         }
+        loading.add(SESSIONS_CHECK_LOADING_ID, Promise.all(loadingPromises), {
+          onSuccess: () => {
+            logger.debug("All sessions verified.");
+            this.isInitialized = true;
+          },
+        });
       },
     });
   }
